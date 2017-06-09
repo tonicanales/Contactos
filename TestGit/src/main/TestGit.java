7723782ee59739e4f674043d47f63648cab3b664
alumne <alumne@PC09S21>
2017-06-09 14:13:15 +0200

    Segundo Cambio

--- TestGit/src/main/TestGit.java
+++ TestGit/src/main/TestGit.java
@@ -5,7 +5,7 @@ public class TestGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Hola mundo 1");
+		System.out.println("Hola mundo 3");
 		
 	}
 
